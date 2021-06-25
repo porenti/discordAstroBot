2c8ad1971ff543e82824cb7c79fa5a66fe8eac5e
Сергей <porenti.tv@gmail.com>
2021-06-25 15:23:28 +0500

    refactor/config.py and docs

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from random import randint as random
 from zxc_pars import zxc_pars
 
 
-from KEYS import ds_token,_list_calls,_list_hate_words,_list_for_hate,_night_list,_night_list_a
+from KEYS import ds_token,_hate_list_a
+from config import _night_list,_night_list_a,_hate_list,_list_calls
 
 
 class MyClient(discord.Client):
@@ -22,8 +23,8 @@ class MyClient(discord.Client):
         elif message.content.title() in _night_list:
             await message.channel.send(_night_list_a[random(0,len(_night_list_a)-1)])
         if random(0,100) <= 30:         #То что человек выполняет функцию не означает, что его не надо обозвать
-            if message.author.id in _list_for_hate:
-                await message.channel.send(_list_hate_words[random(0,len(_list_hate_words)-1)])
+            if message.author.id in _hate_list:
+                await message.channel.send(_hate_list_a[random(0,len(_hate_list_a)-1)])
         print(message.author.id, message.content)
 
 
